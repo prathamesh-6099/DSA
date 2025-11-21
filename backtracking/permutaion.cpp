4fc81b50b56df4e4e25bb00f8b48fe1d5bda83be
prathamesh-6099 <prathameshkolhe6099@gmail.com>
2025-11-21 22:32:05 +0530

    permutations by backtracking

---
+++ backtracking/permutaion.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include <string>
+using namespace std;
+
+
+void permutaion(string str,string ans){
+    if(str.size()==0){
+        cout<<ans<<endl;
+    }
+    int sz=str.size();
+
+    for(int i=0; i<str.size();i++){
+         char chr=str[i];
+         string next_str=str.substr(0,i)+ str.substr(i+1,sz-i-1);
+        permutaion(next_str,ans+chr);
+    }
+}
+
+int main(){
+string str="abc";
+string ans="";
+permutaion(str,ans);
+
+    return 0;
+}
